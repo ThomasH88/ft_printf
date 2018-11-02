6c2dbb6b509ebb52b3739265e7b1b5fae15a25b9
Thomas Holzheu <tholzheu@e1z2r4p32.42.us.org>
2018-11-02 15:07:51 -0700

    fixed some bugs

--- padding.c
+++ padding.c
@@ -6,7 +6,7 @@
 /*   By: tholzheu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/01 09:17:27 by tholzheu          #+#    #+#             */
-/*   Updated: 2018/11/02 14:28:49 by tholzheu         ###   ########.fr       */
+/*   Updated: 2018/11/02 15:04:48 by tholzheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,35 @@ static void		padding_adjustments(t_params *params, t_book *book)
 		book->width--;
 	if (book->width > book->prec && param_is_on(PW0, &params->flags))
 	{
-		if (params->type == PW2)
+		if (params->type == PW2 && book->width > 0)
 			book->width--;
-		else if (params->type == PW3 || params->type == PW4 || params->type == PW5)
+		else if ((params->type == PW3 || params->type == PW4 || params->type == PW5) && book->width > 1)
 			book->width -= 2;
 	}
 	if (param_is_on(PW1, &params->flags))
 		book->prec = book->width;
 }
 
-void		padding_left(t_params *params, t_book *book, size_t len, int *count)
+
+static void		print_prefix(t_params *params, t_book *book, int *count)
+{
+	if (param_is_on(PW3, &params->flags) && params->flags < PW6)
+	{
+		putchar_printf(' ', count);
+		if (book->width > 0)
+			book->width--;
+	}
+	if (param_is_on(PW6, &params->flags))
+		putchar_printf('-', count);
+	else if (param_is_on(PW4, &params->flags))
+		putchar_printf('+', count);
+	else if (param_is_on(PW0, &params->flags) && params->type == PW2)
+		putchar_printf('0', count);
+	else if (param_is_on(PW0, &params->flags) && params->type >= PW3 && params->type <= PW5)
+		putstr_printf("0x", count);
+}
+
+void			padding_left(t_params *params, t_book *book, size_t len, int *count)
 {
 	size_t	tmp;
 
@@ -40,16 +59,7 @@ void		padding_left(t_params *params, t_book *book, size_t len, int *count)
 		putchar_printf(' ', count);
 		book->width--;
 	}
-	if (param_is_on(PW3, &params->flags) && params->flags < PW6 && (book->width--))
-		putchar_printf(' ', count);
-	if (param_is_on(PW6, &params->flags))
-		putchar_printf('-', count);
-	else if (param_is_on(PW4, &params->flags))
-		putchar_printf('+', count);
-	else if (param_is_on(PW0, &params->flags) && params->type == PW2)
-		putchar_printf('0', count);
-	else if (param_is_on(PW0, &params->flags) && params->type >= PW3 && params->type <= PW5)
-		putstr_printf("0x", count);
+	print_prefix(params, book, count);
 	tmp = book->prec;
 	while (tmp > len)
 	{
